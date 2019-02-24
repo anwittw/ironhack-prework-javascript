b49101e01d7a5ec8ff15959fcea0a7e047096c16
anwittw <git@andrewittwer.de>
2019-02-24 12:22:59 +0100

    added properties to rover and a 'updateCurrentPosition' method

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,29 @@
 // Rover Object Goes Here
 // ======================
-
+let rover = {
+  direction: ["W", "N", "E", "S"],
+  x: 0,
+  y: 0,
+  currentPosition: [0,0],
+  updateCurrentPosition: function() {
+    this.currentPosition[0] = this.x;
+    this.currentPosition[1] = this.y;
+    console.log('The current Position is:' + ' x:' + this.x + ' y:' + this.y)
+    console.log(this.currentPosition);
+  }
+}
 // ======================
-function turnLeft(rover){
+function turnLeft(rover) {
+
   console.log("turnLeft was called!");
 }
 
-function turnRight(rover){
+function turnRight(rover) {
   console.log("turnRight was called!");
 }
 
-function moveForward(rover){
+function moveForward(rover) {
   console.log("moveForward was called")
 }
+
+rover.updateCurrentPosition()
